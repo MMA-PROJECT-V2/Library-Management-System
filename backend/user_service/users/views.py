983e59225ad3877af6e26844f6c5624ec7409167
Abdelhafidh Amrane <abdelhafidh.amrane@gmail.com>
2025-11-19 18:03:31 +0100

    register view

--- backend/user_service/users/views.py
+++ backend/user_service/users/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework.response import Response
 
-# Create your views here.
+
+from .models import User, UserProfile
+from .serializers import (
+    UserSerializer,
+    UserProfileSerializer,
+    RegisterSerializer,
+    LoginSerializer
+)
+
+# ======================
+#    🔹 REGISTER
+# ======================
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+    serializer = RegisterSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    user = serializer.save()
+    return Response({
+        "message": "Inscription réussie.",
+        "user": UserSerializer(user).data
+    })
