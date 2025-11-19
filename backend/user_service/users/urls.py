d9d904de8e7b9230f960a65c02f75f3c2c2885a8
Aymen Zahed <aymenzahed62@gmail.com>
2025-11-20 00:06:01 +0100

    add the urls token and token-refresh

--- backend/user_service/users/urls.py
+++ backend/user_service/users/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .views import register, login_view, Me, UserProfileView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 
 urlpatterns = [
@@ -7,4 +12,8 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('me/', Me, name='me'),
     path('profile/', UserProfileView, name='user-profile'),
+        # JWT Provided by SimpleJWT
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
 ]
