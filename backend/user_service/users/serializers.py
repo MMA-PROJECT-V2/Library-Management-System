237a8fcf0841e7133e3220bc953afb47a4bcc5e5
Abdelhafidh Amrane <abdelhafidh.amrane@gmail.com>
2025-11-19 18:00:28 +0100

    register serializer

--- backend/user_service/users/serializers.py
+++ backend/user_service/users/serializers.py
@@ -16,3 +16,28 @@ class UserSerializer(serializers.ModelSerializer):
             "last_name", "phone", "role", "is_active",
             "max_loans", "created_at", "updated_at"
         ]
+
+class RegisterSerializer(serializers.ModelSerializer):
+    """Serializer utilisé pour l'inscription"""
+
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "email", "username", "password",
+            "first_name", "last_name", "phone"
+        ]
+
+    def validate_password(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Le mot de passe doit contenir au moins 8 caractères.")
+        return value
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
